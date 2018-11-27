fb650e21f5f6169f29003cba38c55e99f30bd53f
CerulanLumina <CerulanLumina@users.noreply.github.com>
2018-11-27 14:38:34 -0500

    Revert "add note"
    This reverts commit 1fa596de91c119f795c0b1228cb570559ffa8948.

--- src/lib.rs
+++ src/lib.rs
@@ -67,7 +67,6 @@ enum FileType {
 
 impl FileType {
     pub fn from_expath_string(expath_str: &String) -> Result<FileType, FFXIVError> {
-
         let lower = val.to_ascii_lowercase();
         let spls: &str = lower.split("/").next();
         match spls {
@@ -92,12 +91,9 @@ impl FileType {
     }
 
     pub fn get_sqpack_code(&self) -> String {
-        
         use FileType::*;
         match self {
-            Common => String::from("") // file name should be 0?0000.{index,dat} where the ? corresponds to file type
-            // but, in the actual files, the 4 zeroes afterwards also seem to mean something, but I do not know what.
-            // more research will be necessary
+            Common => String::from("")
         }
     }
 }
